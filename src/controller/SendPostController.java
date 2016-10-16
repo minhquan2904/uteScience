dbd1b4115009f3aae91fe5185b9e2f825f97c2f3
DuongVu <binhbinh0708@gmail.com>
2016-10-16 22:32:05 +0700

    Test

---
+++ src/controller/SendPostController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/listTester.html")
+public class SendPostController extends HttpServlet {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		req.getRequestDispatcher("/views/site/about.jsp").forward(req, resp);
+	}
+	
+}
